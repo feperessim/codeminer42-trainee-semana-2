355ca845aff901eb0d0a556a1f9c34cb7f88e062
Felipe Peressim <fe.peressim@gmail.com>
2022-01-18 23:22:25 -0300

    Update spec/parser_spec.rb
    Co-authored-by: João Pedro <pedrochaveslimas3@gmail.com>

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,7 +2,7 @@ require_relative "../parser.rb"
 
 FILENAME_WITH_PATH = "games_logs_files/games.log"
 
-describe ParserGamesLogs  do
+describe ParserGamesLogs do
   it "Raises exceptions when the file does not exist or when the file header is invalid" do
     expect { 
       parser = ParserGamesLogs.new(FILENAME_WITH_PATH)
