83dd4d7e04557c411fa50ebc7f30b352c87d5ce9
Felipe <fe.peressim@gmail.com>
2022-01-20 17:08:45 -0300

    Update: Adding the suggested improvements at the review of task 2.

--- parser.rb
+++ parser.rb
@@ -4,24 +4,15 @@ class ParserGamesLogs
   def initialize(filename_with_path)
     @filename_with_path = filename_with_path
     @mode = 'r'
-    file_exists?()
+    file_exists()
   end
   
 
   def read_first_line()
-    if file_exists?
+    if file_exists
       File.open(@filename_with_path, @mode) do |log_file|
         log_file.readline
       end
-    end
-  end
-
-
-  def count_lines()
-    if file_exists?
-      count = 0
-      File.foreach(@filename_with_path) { count += 1 }
-      return count
     end
   end
 
@@ -35,8 +26,17 @@ class ParserGamesLogs
 
   private
   
-  def file_exists?()
+  def file_exists()
     raise Errno::ENOENT unless File.exist?(@filename_with_path)
     return true
+  end
+
+
+  def count_lines()
+    if file_exists
+      count = 0
+      File.foreach(@filename_with_path) { count += 1 }
+      return count
+    end
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -11,27 +11,23 @@ describe ParserGamesLogs do
     expect {parser = ParserGamesLogs.new(FILENAME_WITH_PATH)}.not_to raise_error
   end
 
-  it "Checks whether the first line match with the one in the 'games.log' if the file exists" do
-    expect {
-      parser = ParserGamesLogs.new(FILENAME_WITH_PATH)
-      first_line = parser.read_first_line()
-      expect(first_line).to eq(FIRST_LINE)          
-    }.not_to raise_error
+  describe '#read_first_line' do
+    it "Checks whether the first line match with the one in the 'games.log' if the file exists" do
+      expect {
+        parser = ParserGamesLogs.new(FILENAME_WITH_PATH)
+        first_line = parser.read_first_line()
+        expect(first_line).to eq(FIRST_LINE)          
+      }.not_to raise_error
+    end
   end
 
-    it "Counts the total number of lines into the file if it exists." do
-    expect {
-      parser = ParserGamesLogs.new(FILENAME_WITH_PATH)
-      count = parser.count_lines()
-      expect(count).to eq(TOTAL_NUMBER_OF_LINES)
-    }.not_to raise_error
-  end
-
-  it "Expects a json object with contents when parsing the file if it exists." do
-    expect {
-      parser = ParserGamesLogs.new(FILENAME_WITH_PATH)
-      json_parsed_file = JSON.parse(parser.parse_file())
-      expect(json_parsed_file).to include( "#{FILENAME_WITH_PATH}" => {"lines"=> TOTAL_NUMBER_OF_LINES})
-    }.not_to raise_error
+  describe '#parse_file' do
+    it "Expects a json object with contents when parsing the file if it exists." do
+      expect {
+        parser = ParserGamesLogs.new(FILENAME_WITH_PATH)
+        json_parsed_file = JSON.parse(parser.parse_file())
+        expect(json_parsed_file).to include( "#{FILENAME_WITH_PATH}" => {"lines"=> TOTAL_NUMBER_OF_LINES})
+      }.not_to raise_error
+    end
   end
 end
