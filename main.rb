f2da1c7a763e0f4638c3d86e677ae9d70ade901b
Felipe <fe.peressim@gmail.com>
2022-02-02 12:47:58 -0300

    Adding improvements to parser.rb and adding suggested changes by rubocop.

--- main.rb
+++ main.rb
@@ -1,17 +1,17 @@
-require_relative "parser.rb"
+# frozen_string_literal: true
 
+require_relative 'parser'
 
 def main
-  filename_with_path = "games_logs_files/games.log"
+  filename_with_path = 'games_logs_files/games.log'
   begin
     parser = ParserGamesLogs.new(filename_with_path)
-  rescue
-    puts "The class could not be instantiated because the file does not exist."
+  rescue StandardError
+    puts 'The class could not be instantiated because the file does not exist.'
   else
     puts(parser.read_first_line)
     puts(JSON.pretty_generate(JSON.parse(parser.parse_file)))
   end
 end
-
 
 main
--- parser.rb
+++ parser.rb
@@ -1,62 +1,61 @@
+# frozen_string_literal: true
+
 require 'json'
 
+class ParserGamesLogs
+  IGNORED_PLAYER = '<world>'
+  RE_PATTERN_PLAYERS = /(?:Kill:.*:) (.*) (?:killed) (.*) (?:by)|(?:ClientUserinfoChanged:.*n\\)(.*)(?:\\t\\0\\model.*)/
+  RE_PATTERN_KILL = /(?:Kill:.*:) (.*) (?:killed) (.*) (?:by)/
 
-class ParserGamesLogs 
   def initialize(filename_with_path)
     raise Errno::ENOENT unless File.exist?(filename_with_path)
+
     @filename_with_path = filename_with_path
   end
-  
 
   def read_first_line
-    File.open(@filename_with_path) do |log_file|
-      log_file.readline
-    end
+    File.open(@filename_with_path, &:readline)
   end
-
 
   def parse_file
     all_kills, total_kills = parse_kills
     { @filename_with_path => {
-        "lines" => count_lines,
-        "players" => parse_players,
-        "kills" => all_kills,
-        "total_kills" => total_kills } }.to_json
+      'lines' => count_lines,
+      'players' => parse_players,
+      'kills' => all_kills,
+      'total_kills' => total_kills
+    } }.to_json
   end
-  
-  
+
   private
-  
+
   def count_lines
     File.readlines(@filename_with_path).length
   end
 
-
   def parse_players
-    re_pattern_players = /(?:Kill:.*:) (.*) (?:killed) (.*) (?:by)|(?:ClientUserinfoChanged:.*n\\)(.*)(?:\\t\\0\\model.*)/
-    
     File.readlines(@filename_with_path).flat_map do |line|
-      next unless matches = line.match(re_pattern_players)
+      next unless matches = line.match(RE_PATTERN_PLAYERS)
+
       matches.captures
-    end.compact.uniq - ['<world>']
+    end.compact.uniq - [IGNORED_PLAYER]
   end
 
-  
   def parse_kills
-    re_pattern_kill = /(?:Kill:.*:) (.*) (?:killed) (.*) (?:by)/
     players = parse_players
-    initial_kills = Hash[players.map{|player| [player, 0]}]
-    
+    initial_kills = Hash[players.map { |player| [player, 0] }]
+
     File.readlines(@filename_with_path).reduce([initial_kills, 0]) do |(kills, total), line|
-      next [kills, total] unless matches = line.match(re_pattern_kill)
+      next [kills, total] unless matches = line.match(RE_PATTERN_KILL)
+
       killer, killed = matches.captures
       [kills.merge(counting_kill(killer, kills)), total + 1]
     end
   end
 
-  
   def counting_kill(killer, kills)
-    return kills if killer == "<world>"
+    return kills if killer == IGNORED_PLAYER
+
     { killer => kills[killer] + 1 }
   end
 end
