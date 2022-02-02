a6a06b37e7293e8856ed11ba52ed981df54a1c25
Felipe <fe.peressim@gmail.com>
2022-02-02 12:08:57 -0300

    Adding newline after let expression and adding corrections suggested by rubocop

--- parser.rb
+++ parser.rb
@@ -1,39 +1,36 @@
+# frozen_string_literal: true
+
 require 'json'
 
-
-class ParserGamesLogs 
+class ParserGamesLogs
   def initialize(filename_with_path)
     @filename_with_path = filename_with_path
     raise Errno::ENOENT unless File.exist?(@filename_with_path)
   end
-  
 
   def read_first_line
-    File.open(@filename_with_path) do |log_file|
-      log_file.readline
-    end
+    File.open(@filename_with_path, &:readline)
   end
-
 
   def parse_file
     { @filename_with_path => {
-        "lines" => count_lines,
-        "players" => parse_players } }.to_json
+      'lines' => count_lines,
+      'players' => parse_players
+    } }.to_json
   end
-  
-  
+
   private
-  
+
   def count_lines
     File.readlines(@filename_with_path).length
   end
 
-
   def parse_players
     re_pattern_players = /(?:Kill:.*:) (.*) (?:killed) (.*) (?:by)|(?:ClientUserinfoChanged:.*n\\)(.*)(?:\\t\\0\\model.*)/
-    
+
     File.readlines(@filename_with_path).flat_map do |line|
       next unless matches = line.match(re_pattern_players)
+
       matches.captures
     end.compact.uniq - ['<world>']
   end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,17 +1,19 @@
-require_relative "../parser.rb"
+# frozen_string_literal: true
+
+require_relative '../parser'
 require 'json'
 
-
 describe ParserGamesLogs do
-  let(:filename_with_path) { "games_logs_files/games.log" }
-  context "When the file exists" do
-    it "Instantiates the class." do
+  let(:filename_with_path) { 'games_logs_files/games.log' }
+
+  context 'When the file exists' do
+    it 'Instantiates the class.' do
       expect { ParserGamesLogs.new(filename_with_path) }.not_to raise_error
     end
 
-
     describe '#read_first_line' do
       let(:first_line) { "  0:00 ------------------------------------------------------------\n" }
+
       it "Returns the first line of the file 'games.log'" do
         parser = ParserGamesLogs.new(filename_with_path)
         parser_first_line = parser.read_first_line
@@ -19,26 +21,29 @@ describe ParserGamesLogs do
       end
     end
 
-
     describe '#parse_file' do
-      let(:total_number_of_lines) {5306}
-      let(:players) {[
-                       "Isgalamido", "Dono da Bola", "Mocinha", "Zeh", "Assasinu Credi", "Fasano Again", 
-                       "Oootsimo", "UnnamedPlayer", "Maluquinho", "Mal",  "Chessus!", "Chessus"
-                     ]}
-      it "Returns a json object after parses the file." do
+      let(:total_number_of_lines) { 5306 }
+      let(:players) do
+        [
+          'Isgalamido', 'Dono da Bola', 'Mocinha', 'Zeh', 'Assasinu Credi', 'Fasano Again',
+          'Oootsimo', 'UnnamedPlayer', 'Maluquinho', 'Mal', 'Chessus!', 'Chessus'
+        ]
+      end
+
+      it 'Returns a json object after parses the file.' do
         parser = ParserGamesLogs.new(filename_with_path)
         json_parsed_file = JSON.parse(parser.parse_file)
-        expect(json_parsed_file).to include("#{filename_with_path}" => { "lines"=> total_number_of_lines, "players"=> players })
+        expect(json_parsed_file).to include(filename_with_path.to_s => { 'lines' => total_number_of_lines,
+                                                                         'players' => players })
       end
     end
   end
-  
-  
-  context "When the file exists" do
-    let(:wrong_filename_with_path) {"wrong/path/games.log"}
-    it "Raises an Errno::ENOENT." do
-      expect {ParserGamesLogs.new(wrong_filename_with_path)}.to raise_error(Errno::ENOENT)
+
+  context 'When the file exists' do
+    let(:wrong_filename_with_path) { 'wrong/path/games.log' }
+
+    it 'Raises an Errno::ENOENT.' do
+      expect { ParserGamesLogs.new(wrong_filename_with_path) }.to raise_error(Errno::ENOENT)
     end
   end
 end
