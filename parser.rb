15404475dd4fa702fe106cb33694fb60d2928c9e
Felipe <fe.peressim@gmail.com>
2022-02-02 12:18:23 -0300

    Fix: Putting each let inside its corresponding describe and putting a newline after each let. Also adding some corrections suggested by rubocop

--- parser.rb
+++ parser.rb
@@ -1,62 +1,60 @@
+# frozen_string_literal: true
+
 require 'json'
 
-
-class ParserGamesLogs 
+class ParserGamesLogs
   def initialize(filename_with_path)
     raise Errno::ENOENT unless File.exist?(filename_with_path)
+
     @filename_with_path = filename_with_path
   end
-  
 
   def read_first_line
-    File.open(@filename_with_path) do |log_file|
-      log_file.readline
-    end
+    File.open(@filename_with_path, &:readline)
   end
-
 
   def parse_file
     all_kills, total_kills = parse_kills
     { @filename_with_path => {
-        "lines" => count_lines,
-        "players" => parse_players,
-        "kills" => all_kills,
-        "total_kills" => total_kills } }.to_json
+      'lines' => count_lines,
+      'players' => parse_players,
+      'kills' => all_kills,
+      'total_kills' => total_kills
+    } }.to_json
   end
-  
-  
+
   private
-  
+
   def count_lines
     File.readlines(@filename_with_path).length
   end
 
-
   def parse_players
     re_pattern_players = /(?:Kill:.*:) (.*) (?:killed) (.*) (?:by)|(?:ClientUserinfoChanged:.*n\\)(.*)(?:\\t\\0\\model.*)/
-    
+
     File.readlines(@filename_with_path).flat_map do |line|
       next unless matches = line.match(re_pattern_players)
+
       matches.captures
     end.compact.uniq - ['<world>']
   end
-  
-  
+
   def parse_kills
     re_pattern_kill = /(?:Kill:.*:) (.*) (?:killed) (.*) (?:by)/
     players = parse_players
-    initial_kills = Hash[players.map{|player| [player, 0]}]
-    
+    initial_kills = Hash[players.map { |player| [player, 0] }]
+
     File.readlines(@filename_with_path).reduce([initial_kills, 0]) do |(kills, total), line|
       next [kills, total] unless matches = line.match(re_pattern_kill)
+
       killer, killed = matches.captures
       [kills.merge(counting_kill(killer, killed, kills)), total + 1]
     end
   end
 
-
   def counting_kill(killer, killed, kills)
-    return { killed => kills[killed] - 1 } if killer == "<world>"
+    return { killed => kills[killed] - 1 } if killer == '<world>'
+
     { killer => kills[killer] + 1 }
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,17 +1,19 @@
-require_relative "../parser.rb"
+# frozen_string_literal: true
+
+require_relative '../parser'
 require 'json'
 
-
 describe ParserGamesLogs do
-  let(:filename_with_path) { "games_logs_files/games.log" }
-  context "When the file exists" do
-    it "Instantiates the class." do
+  let(:filename_with_path) { 'games_logs_files/games.log' }
+
+  context 'When the file exists' do
+    it 'Instantiates the class.' do
       expect { ParserGamesLogs.new(filename_with_path) }.not_to raise_error
     end
 
-
-    let(:first_line) { "  0:00 ------------------------------------------------------------\n" }
     describe '#read_first_line' do
+      let(:first_line) { "  0:00 ------------------------------------------------------------\n" }
+
       it "Returns the first line of the file 'games.log'" do
         parser = ParserGamesLogs.new(filename_with_path)
         parser_first_line = parser.read_first_line
@@ -19,31 +21,36 @@ describe ParserGamesLogs do
       end
     end
 
-
-    let(:total_number_of_lines) {5306}
-    let(:players) {[
-                     "Isgalamido",  "Dono da Bola", "Mocinha", "Zeh", "Assasinu Credi", "Fasano Again",
-                      "Oootsimo", "UnnamedPlayer", "Maluquinho", "Mal",  "Chessus!", "Chessus"
-                   ]}
-    let(:kills) {{
-                   "Isgalamido"=> 147, "Dono da Bola"=> 63, "Mocinha"=> 0, "Zeh"=> 124, "Assasinu Credi"=> 111, "Fasano Again"=> 0,
-                   "Oootsimo"=> 114, "UnnamedPlayer"=> 0, "Maluquinho"=> 0, "Mal"=> -3, "Chessus!"=> 0, "Chessus"=> 33
-                 }}
-    let(:total_kills) {1069}
     describe '#parse_file' do
-      it "Returns a json object after parses the file." do
+      let(:total_number_of_lines) { 5306 }
+      let(:players) do
+        [
+          'Isgalamido', 'Dono da Bola', 'Mocinha', 'Zeh', 'Assasinu Credi', 'Fasano Again',
+          'Oootsimo', 'UnnamedPlayer', 'Maluquinho', 'Mal', 'Chessus!', 'Chessus'
+        ]
+      end
+      let(:kills) do
+        {
+          'Isgalamido' => 147, 'Dono da Bola' => 63, 'Mocinha' => 0, 'Zeh' => 124, 'Assasinu Credi' => 111, 'Fasano Again' => 0,
+          'Oootsimo' => 114, 'UnnamedPlayer' => 0, 'Maluquinho' => 0, 'Mal' => -3, 'Chessus!' => 0, 'Chessus' => 33
+        }
+      end
+      let(:total_kills) { 1069 }
+
+      it 'Returns a json object after parses the file.' do
         parser = ParserGamesLogs.new(filename_with_path)
         json_parsed_file = JSON.parse(parser.parse_file)
-        expect(json_parsed_file).to include("#{filename_with_path}" => { "lines"=> total_number_of_lines, "players"=> players, "kills"=> kills, "total_kills"=> total_kills })
+        expect(json_parsed_file).to include(filename_with_path.to_s => { 'lines' => total_number_of_lines,
+                                                                         'players' => players, 'kills' => kills, 'total_kills' => total_kills })
       end
     end
   end
 
+  context 'When the file exists' do
+    let(:wrong_filename_with_path) { 'wrong/path/games.log' }
 
-  let(:wrong_filename_with_path) {"wrong/path/games.log"}
-  context "When the file exists" do
-    it "Raises an Errno::ENOENT." do
-      expect {ParserGamesLogs.new(wrong_filename_with_path)}.to raise_error(Errno::ENOENT)
+    it 'Raises an Errno::ENOENT.' do
+      expect { ParserGamesLogs.new(wrong_filename_with_path) }.to raise_error(Errno::ENOENT)
     end
   end
 end
